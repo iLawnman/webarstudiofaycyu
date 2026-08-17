a2cb6a2b469979e8933627dfa8fcd410828630e4
iliasom <i9091780@gmail.com>
2026-08-17 17:31:50 +0545

    about update

---
+++ assets/квест неСказка/screens_data.js
@@ -0,0 +1,46 @@
+const START_SCREENS = [
+  {
+    "id": "SplashScreen",
+    "title": "<size=150>неСказка</size>",
+    "titleColor": "#DAA520",
+    "subtitle": "<size=90>КВЕСТ</size>",
+    "subtitleColor": "#FF4500",
+    "text": "Командная работа сделает мир лучше! Друзья, коллеги, семья - те, кто с тобой в одной лодке, поддержат тебя, и ты поддержи!",
+    "textColor": "#DAA520",
+    "bg": "SSBG1024",
+    "logo": "R4QuestLogoWhite512"
+  },
+  {
+    "id": "Legend1",
+    "title": "неСказка начинается",
+    "titleColor": "#FFFFFF",
+    "subtitle": "",
+    "subtitleColor": "#FF4500",
+    "text": "Рекламное агентсво в городе небольшом, и ты с Аленкой в нем ведешь стратегические проекты. Город как обычный, но не совсем, далеко он от Столицы, и сохранилось здесь сказочное. Немного волшебства, чудеса!",
+    "textColor": "#FFFFFF",
+    "bg": "LegendBG",
+    "logo": "R4QuestLogoWhite512"
+  },
+  {
+    "id": "Legend2",
+    "title": "Что случилось",
+    "titleColor": "#FFFFFF",
+    "subtitle": "",
+    "subtitleColor": "#FF4500",
+    "text": "Аленка потерялась, а это странно, ведь через 2 часа съемка для клиента!",
+    "textColor": "#FFFFFF",
+    "bg": "LegendBG",
+    "logo": "R4QuestLogoWhite512"
+  },
+  {
+    "id": "Legend3",
+    "title": "Что делать?",
+    "titleColor": "#FFFFFF",
+    "subtitle": "",
+    "subtitleColor": "#FF4500",
+    "text": "Найди Аленку, спаси друга и проект!",
+    "textColor": "#FFFFFF",
+    "bg": "LegendBG",
+    "logo": "R4QuestLogoWhite512"
+  }
+];
